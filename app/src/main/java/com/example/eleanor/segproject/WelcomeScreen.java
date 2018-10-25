2b413542af6f2f5b3b619c552c1065c4a42edafe
Eleanor-Rumsey <erums071@uottawa.ca>
2018-10-25 19:25:36 -0400

    changes to manifest

--- app/src/main/java/com/example/eleanor/segproject/WelcomeScreen.java
+++ app/src/main/java/com/example/eleanor/segproject/WelcomeScreen.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class WelcomeScreen extends HomeOwner{
+public class WelcomeScreen extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
