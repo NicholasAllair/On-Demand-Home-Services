274c55aeb27db4d82795001e8bf2bcd565fae4d1
Eleanor-Rumsey <erums071@uottawa.ca>
2018-10-25 18:48:19 -0400

    changes

--- app/src/main/java/com/example/eleanor/segproject/HomeOwner.java
+++ app/src/main/java/com/example/eleanor/segproject/HomeOwner.java
@@ -5,19 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import fieldvalidators.ProfileValidator;
 import fieldvalidators.ProfileValidatorFactory;
 
 public class HomeOwner extends AppCompatActivity {
     Button createProfile;
+    EditText editName, editEmail, editPassword;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_owner);
 
+        editName = (EditText) findViewById(R.id.EnterName);
+        editEmail = (EditText) findViewById(R.id.EnterEmail);
+        editPassword = (EditText) findViewById(R.id.EnterPassword);
         createProfile = (Button) findViewById(R.id.ReturnHome);
+
 
         createProfile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,9 +34,9 @@ public class HomeOwner extends AppCompatActivity {
                         ProfileValidator.class);
                 startActivity(validate);
 
-                /*Intent returnHome = new Intent(HomeOwner.this,
-                        MainActivity.class);
-                startActivity(returnHome); */
+                Intent returnHome = new Intent(HomeOwner.this,
+                        WelcomeScreen.class);
+                startActivity(returnHome);
             }
         });
     }
--- app/src/main/java/com/example/eleanor/segproject/WelcomeScreen.java
+++ app/src/main/java/com/example/eleanor/segproject/WelcomeScreen.java
@@ -1,4 +1,21 @@
 package com.example.eleanor.segproject;
 
-public class WelcomeScreen {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class WelcomeScreen extends HomeOwner {
+    String name = editName.getText().toString();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen;
+
+        EditText e (EditText) findViewById 
+
+    }
+
 }
