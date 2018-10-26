d26a77af1b90744ccb4d9ca2f79a058befbe1f28
Nicholas  Allair <nallair5@gmail.com>
2018-10-25 20:26:55 -0400

    nick

--- app/src/main/java/com/example/eleanor/segproject/ServiceProvider.java
+++ app/src/main/java/com/example/eleanor/segproject/ServiceProvider.java
@@ -1,4 +1,19 @@
 package com.example.eleanor.segproject;
 
-public class ServiceProvider {
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import fieldvalidators.ProfileValidator;
+import fieldvalidators.ProfileValidatorFactory;
+
+public class ServiceProvider extends AppCompatActivity {
+
+
+
+
 }
