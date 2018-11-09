37a8853709e45d0c61cd48429365091259d0eb8b
Eleanor-Rumsey <erums071@uottawa.ca>
2018-11-08 19:32:52 -0500

    Buttons fixed! no validation but its okay

--- app/src/main/java/com/example/eleanor/segproject/HomeOwner.java
+++ app/src/main/java/com/example/eleanor/segproject/HomeOwner.java
@@ -4,13 +4,7 @@ import android.content.Intent;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.SpannableStringBuilder;
-import android.text.TextWatcher;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 
 public class HomeOwner extends User{
@@ -22,56 +16,14 @@ public class HomeOwner extends User{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homeowner_homepage);
-
-        editName = findViewById(R.id.EnterName);
-        setName(editName);
-
-        editEmail = findViewById(R.id.EnterEmail);
-        setEmail(editEmail);
-
-
-        editPassword = findViewById(R.id.EnterPassword);
-
-        setPassword(editPassword);
-
-        createProfile = findViewById(R.id.ReturnHome);
-
-        final String invalidEmailMessage = "Email address invalid";
-        final String invalidUserNameMessage = "Username invalid";
-
-/*        createProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent returnHome = new Intent(HomeOwner.this, HomeOwnerWelcome.class);
-                /*
-                do {
-                    TextView invalidEmail = findViewById(R.id.InvalidEmail);
-                    invalidEmail.setText(invalidEmailMessage);
-                }while(!isValidEmail(editEmail.getText().toString()));
-
-                do {
-                    TextView invalidUser = findViewById(R.id.InvalidUsername);
-                    invalidUser.setText(invalidUserNameMessage);
-                }while(!isValidUsername(editName.getText().toString()));
-
-
-                startActivity(returnHome);
-            }
-        });
-*/
-
-        createProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent returnHome = new Intent(HomeOwner.this,
-                        ServiceProviderWelcome.class);
-                startActivity(returnHome);
-            }
-        });
-
+        setContentView(R.layout.activity_home_owner_create);
     }
+
+    public void onClickCreateHOProfile(){
+        Intent HOIntent = new Intent(HomeOwner.this, HomeOwnerWelcome.class);
+        startActivity(HOIntent);
+    }
+
     public String getName(){ return editName.getText().toString(); }
     public void setName(EditText name){
         this.editName = name;
--- app/src/main/java/com/example/eleanor/segproject/ServiceProvider.java
+++ app/src/main/java/com/example/eleanor/segproject/ServiceProvider.java
@@ -3,40 +3,19 @@ package com.example.eleanor.segproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 
 public class ServiceProvider extends AppCompatActivity {
-    Button createSPProfile;
-    EditText editName, editEmail, editServiceType, editPassword;
-    TextView result;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_provider_create);
-
-        editName = findViewById(R.id.EnterSPName);
-        editServiceType = findViewById(R.id.ServiceType);
-        editEmail = findViewById(R.id.EnterSPEmail);
-        editPassword = findViewById(R.id.EnterSPPassword);
-        createSPProfile = findViewById(R.id.SPReturnHome);
-
-        createSPProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent returnHome = new Intent(ServiceProvider.this,
-                        ServiceProviderWelcome.class);
-                startActivity(returnHome);
-            }
-        });
     }
 
     public void onClickCreateSPProfile(){
-        
+        Intent returnHome = new Intent(ServiceProvider.this, ServiceProviderWelcome.class);
+        startActivity(returnHome);
     }
 }
