d7f97b4f4cb432ffdd83a531fcd583da76a40c09
Eleanor-Rumsey <erums071@uottawa.ca>
2018-11-09 13:21:02 -0500

    AddService UI

--- app/src/main/java/com/example/eleanor/segproject/AddService.java
+++ app/src/main/java/com/example/eleanor/segproject/AddService.java
@@ -1,7 +1,9 @@
 package com.example.eleanor.segproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 
 public class AddService extends AppCompatActivity {
@@ -9,7 +11,12 @@ public class AddService extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_administrator);
+        setContentView(R.layout.activity_add_service);
 
+    }
+
+    public void onNewServiceClick(View view){
+        Intent HOIntent = new Intent(AddService.this, Admin.class);
+        startActivity(HOIntent);
     }
 }
