9428fc233a9635c0e9b0ccb06a72abb0a4019c4e
Eleanor-Rumsey <erums071@uottawa.ca>
2018-11-09 14:35:57 -0500

    Creating list

---
+++ app/src/main/java/com/example/eleanor/segproject/ViewServices.java
@@ -0,0 +1,22 @@
+package com.example.eleanor.segproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class ViewServices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_services);
+
+    }
+
+    public void printServiceList(ServiceList list){
+
+    }
+
+}
