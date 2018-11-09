b72550f0415dc1dc79c70e5805873df415100d9d
Nicholas  Allair <nallair5@gmail.com>
2018-11-08 19:03:16 -0500

    service class

--- app/src/main/java/com/example/eleanor/segproject/Service.java
+++ app/src/main/java/com/example/eleanor/segproject/Service.java
@@ -1,4 +1,27 @@
 package com.example.eleanor.segproject;
 
 public class Service {
+
+    String serviceName;//string variable to hold name of service
+    double serviceRate;//double variable to hold hourly rate of the service
+
+    public Service (String s, double d ){//constructor for a new service given a name and pay rate
+
+        serviceName = s;
+        serviceRate = d;
+
+    }
+
+
+    //setter methods for service name & rate, (allows admin to edit the service)
+    public void setServiceName(String s){ serviceName = s; }
+
+    public void setServiceRate(double d){ serviceRate = d; }
+
+    //getter methods for service name & rate
+
+    public String getServiceName(){ return serviceName; }
+
+    public double getServiceRate(){ return serviceRate; }
+
 }
