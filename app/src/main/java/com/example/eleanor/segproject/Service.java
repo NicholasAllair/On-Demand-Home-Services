ed12974c3f75134291fc98d973d40f68bdb7c5d8
Nicholas  Allair <nallair5@gmail.com>
2018-11-09 13:21:37 -0500

    add method

--- app/src/main/java/com/example/eleanor/segproject/Service.java
+++ app/src/main/java/com/example/eleanor/segproject/Service.java
@@ -1,6 +1,15 @@
 package com.example.eleanor.segproject;
 
-public class Service {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Service extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 
     String serviceName;//string variable to hold name of service
     double serviceRate;//double variable to hold hourly rate of the service
--- app/src/main/java/com/example/eleanor/segproject/ServiceList.java
+++ app/src/main/java/com/example/eleanor/segproject/ServiceList.java
@@ -3,13 +3,31 @@ package com.example.eleanor.segproject;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.ArrayList;
 
 public class ServiceList extends AppCompatActivity {
+
+    public ArrayList<Service> LISTOFSERVICES;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_administrator);
+    }
+
+    public void addService(String serviceName, double payrate){
+        Service aService = new Service(serviceName, payrate);
+        LISTOFSERVICES.add(aService);
+    }
+
+    public void removeService(String serviceName){
+
 
     }
+
+    public void editService(String servicename){
+
+    }
+
+
+
 }
