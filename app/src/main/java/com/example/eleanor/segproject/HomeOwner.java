71f2e4e0a97fa3690a0c83db03b09052a7820ca0
Eleanor-Rumsey <erums071@uottawa.ca>
2018-11-09 11:00:24 -0500

    Show username on welcome screen

--- app/src/main/java/com/example/eleanor/segproject/HomeOwner.java
+++ app/src/main/java/com/example/eleanor/segproject/HomeOwner.java
@@ -12,9 +12,12 @@ import org.w3c.dom.Text;
 
 
 public class HomeOwner extends User{
-    EditText name;
-    EditText email;
-    EditText password;
+    public static String HONAME;
+
+    EditText editName;
+    EditText editEmail;
+    EditText editPassword;
+
 
 
     public HomeOwner(){
@@ -41,30 +44,32 @@ public class HomeOwner extends User{
             Intent HOIntent = new Intent(HomeOwner.this, HomeOwnerWelcome.class);
             startActivity(HOIntent);
         }
+
+        HONAME = getName();
     }
 
     public String getName(){
-        name = findViewById(R.id.EnterName);
-        return name.getText().toString();
+        editName = findViewById(R.id.EnterName);
+        return editName.getText().toString();
     }
-    public void setName(EditText name){
-        this.name = name;
+    public void setName(EditText editName){
+        this.editName = editName;
     }
 
     public String getEmail(){
-        email = findViewById(R.id.EnterEmail);
-        return email.getText().toString();
+        editEmail = findViewById(R.id.EnterEmail);
+        return editEmail.getText().toString();
     }
-    public void setEmail(EditText email){
-        this.email = email;
+    public void setEmail(EditText editEmail){
+        this.editEmail = editEmail;
     }
 
     public String getPassword(){
-        password = findViewById(R.id.EnterPassword);
-        return password.getText().toString();
+        editPassword = findViewById(R.id.EnterPassword);
+        return editPassword.getText().toString();
     }
     public void setPassword(EditText password){
-        this.password = password;
+        this.editPassword = password;
     }
 
 }
--- app/src/main/java/com/example/eleanor/segproject/HomeOwnerWelcome.java
+++ app/src/main/java/com/example/eleanor/segproject/HomeOwnerWelcome.java
@@ -2,10 +2,14 @@ package com.example.eleanor.segproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import static com.example.eleanor.segproject.HomeOwner.HONAME;
 
 public class HomeOwnerWelcome extends AppCompatActivity{
     TextView username;
@@ -15,7 +19,7 @@ public class HomeOwnerWelcome extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homeowner_homepage);
 
-        setUserName(getUserName());
+        setUserName(HONAME);
     }
 
     public String getUserName(){
