5918de5eb15fe648925c45f6cd480a69c4926cc3
Eleanor-Rumsey <erums071@uottawa.ca>
2018-11-19 19:52:11 -0500

    work

--- app/src/main/java/com/example/eleanor/segproject/SpecifyAvailability.java
+++ app/src/main/java/com/example/eleanor/segproject/SpecifyAvailability.java
@@ -6,16 +6,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 
 public class SpecifyAvailability extends AppCompatActivity {
     Button addAvailability = findViewById(R.id.addAvail);
+    EditText startTime = findViewById(R.id.startTime);
+    EditText endTime = findViewById(R.id.endTime);
+    ArrayList<String> availabilities;
+    Spinner day = findViewById(R.id.daySpinner);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +37,7 @@ public class SpecifyAvailability extends AppCompatActivity {
     }
 
     public void onAddAvailClick(){
-
+        availabilities.add(availabilityToString(this.day))
     }
 
     public static String formatDate(long milliSeconds, String dateFormat)
@@ -41,6 +47,10 @@ public class SpecifyAvailability extends AppCompatActivity {
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(milliSeconds);
         return formatter.format(calendar.getTime());
+    }
+
+    public String availabilityToString(String day, String startTime, String endTime){
+        return startTime + ": " + startTime + " - " + endTime;
     }
 
 }
