629a9cac96798d3b4d9b4cf8021af02be47aa7d5
Eleanor-Rumsey <erums071@uottawa.ca>
2018-11-20 15:51:38 -0500

    last

--- app/src/main/java/com/example/eleanor/segproject/MainActivity.java
+++ app/src/main/java/com/example/eleanor/segproject/MainActivity.java
@@ -13,11 +13,14 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setDatabase();
     }
 
     private void setDatabase(){
