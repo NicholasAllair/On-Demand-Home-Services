2bb2cd6993d6eabf994cd3da1f5114a1f4c40d4a
Eleanor-Rumsey <erums071@uottawa.ca>
2018-11-28 19:31:00 -0500

    Firebase compatibility

--- app/src/main/java/com/example/eleanor/segproject/ViewList.java
+++ app/src/main/java/com/example/eleanor/segproject/ViewList.java
@@ -1,65 +1,181 @@
 package com.example.eleanor.segproject;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import static com.example.eleanor.segproject.ServiceList.LISTOFSERVICES;
+import java.util.Iterator;
 
 public class ViewList extends AppCompatActivity {
-    ListView lv;
+    private ListView dataListView;
+    private EditText itemText;
+    private Button findButton;
+    private Button deleteButton;
+    private Boolean searchMode = false;
+    private Boolean itemSelected = false;
+    private int selectedPosition = 0;
 
-    private DatabaseReference mDatabase;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference dbRef = database.getReference().child("service");
+
+    ArrayList<String> listItems = new ArrayList<String>();
+    ArrayList<String> listKeys = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_list);
 
-        lv = (ListView) findViewById(R.id.serviceListView);
+        dataListView = (ListView) findViewById(R.id.dataListView);
+        itemText = (EditText) findViewById(R.id.itemText);
+        findButton = (Button) findViewById(R.id.findButton);
+        deleteButton = (Button) findViewById(R.id.deleteButton);
+        deleteButton.setEnabled(false);
 
-        ArrayList<String> stringServiceList = new ArrayList<String>();
+        System.out.println("here");
+        System.out.println(listItems);
 
-        for(int i=0; i<LISTOFSERVICES.size(); i++){
-            stringServiceList.add(LISTOFSERVICES.get(i).toString());
-        }
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                this,
-                android.R.layout.simple_list_item_1,
-                stringServiceList );
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_single_choice,
+                listItems);
 
-        lv.setAdapter(arrayAdapter);
+        dataListView.setAdapter(adapter);
+        dataListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        dataListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        selectedPosition = position;
+                        itemSelected = true;
+                        deleteButton.setEnabled(true);
+                    }
+                });
+
+        addChildEventListener();
     }
 
-    public void addFireBaseDatabaseListener() {
-        ValueEventListener postListener = new ValueEventListener() {
+    private void addChildEventListener() {
+        ChildEventListener childListener = new ChildEventListener() {
+
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // Get Post object and use the values to update the UI
-                Service service = dataSnapshot.getValue(Service.class);
-                // ...
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value = (String) dataSnapshot.child("service").getValue();
+                if (value != null) {
+                    adapter.add(value);
+                    listKeys.add(dataSnapshot.getKey());
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                String key = dataSnapshot.getKey();
+                int index = listKeys.indexOf(key);
+
+                if (index != -1) {
+                    listItems.remove(index);
+                    listKeys.remove(index);
+                    adapter.notifyDataSetChanged();
+                }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-                // Getting Post failed, log a message
-                System.out.println(databaseError.getMessage());
             }
         };
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        mDatabase.addListenerForSingleValueEvent(postListener);
+        dbRef.addChildEventListener(childListener);
     }
 
-    public void viewList(){
+    public void addItem(View view) {/*
 
+        String item = itemText.getText().toString();
+        String key = dbRef.push().getKey();
+
+        itemText.setText("");
+        dbRef.child(key).child("service").setValue(item);
+
+        adapter.notifyDataSetChanged(); */
     }
+
+    public void deleteItem(View view) {
+        dataListView.setItemChecked(selectedPosition, false);
+        dbRef.child(listKeys.get(selectedPosition)).removeValue();
+    }
+
+    public void findItems(View view) { /*
+
+        Query query;
+
+        if (!searchMode) {
+            findButton.setText("Clear");
+            query = dbRef.orderByChild("service").equalTo(itemText.getText().toString());
+            searchMode = true;
+        } else {
+            searchMode = false;
+            findButton.setText("Find");
+            query = dbRef.orderByKey();
+        }
+
+        if (itemSelected) {
+            dataListView.setItemChecked(selectedPosition, false);
+            itemSelected = false;
+            deleteButton.setEnabled(false);
+        }
+
+        query.addListenerForSingleValueEvent(queryValueListener);
+    }
+
+    ValueEventListener queryValueListener = new ValueEventListener() {
+
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+
+            Iterable<DataSnapshot> snapshotIterator = dataSnapshot.getChildren();
+            Iterator<DataSnapshot> iterator = snapshotIterator.iterator();
+
+            adapter.clear();
+            listKeys.clear();
+
+            while (iterator.hasNext()) {
+                DataSnapshot next = (DataSnapshot) iterator.next();
+
+                String match = (String) next.child("service").getValue();
+                String key = next.getKey();
+                listKeys.add(key);
+                adapter.add(match);
+            }
+
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        } */
+    };
+
+
 }
